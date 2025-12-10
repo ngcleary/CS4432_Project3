90676882bd8403714442f8cfe8e81bc899a1ffcf
Nora Cleary <120121240+ngcleary@users.noreply.github.com>
2025-12-09 19:01:10 -0500

    section 3

--- NestedLoop.java
+++ NestedLoop.java
@@ -5,49 +5,31 @@ import java.util.Arrays;
 
 public class NestedLoop {
     private char[] buffer;
-    private int joinCount;
+    private int qualifyTotal;
 
     public NestedLoop(){
         buffer = new char[4000];
-        joinCount = 0;
+        qualifyTotal = 0;
     }
 
-    public String fetchRecord(File file, int offset){
-        System.out.println("in fetch record");
-        try (FileReader fr = new FileReader(file)) {
-            int fileIndex = 0;
-            int charInt;
-            String recordStr;
-            char[] fileContent = new char[4000];
-            while((charInt = fr.read()) != -1){
-                fileContent[fileIndex] = (char) charInt;
-                fileIndex++;
-            }
-            //loop through file B by 40 to look at each record in B
-            char[] record = Arrays.copyOfRange(fileContent, offset, offset + 40);
-            //get record
-            recordStr = new String(record);
-            return recordStr;
-        } catch (IOException e) {
-            System.out.print(e.getMessage());
-        }
-        return "";
-    }
-
-    public void joinCondition(int randVB){
-        System.out.println("in joincondition");
+    public int joinCondition(int randVB){
+        //start a count of qualifying records
+        int count = 0;
         //loop through the array of A and see if A randV > B randV
-        for (int offset = 0; offset + 40 <= buffer.length; offset += 40){
+        for (int offset = 0; offset <= 3960; offset += 40){
             String record = String.valueOf(Arrays.copyOfRange(buffer, offset, offset + 40));
             int randVA = Integer.parseInt(record.substring(33, 37));
+            //increase count
             if (randVA > randVB){
-                joinCount++;
+                count++;
             }
         }
+        return count;
     }
     public void readDataset(){
         //loop over every file in A
         File folder = new File("Project3Dataset-A");
+        //File folder = new File("testA");
         File[] files = folder.listFiles((dir, name) -> name.endsWith(".txt")); 
 
         //read each file in A
@@ -61,20 +43,35 @@ public class NestedLoop {
                         buffer[fileIndex] = (char) charInt;
                         fileIndex++;
                     }
-                    //read dataset B - redundant code fix later
                     File folderB = new File("Project3Dataset-B");
                     File[] filesB = folderB.listFiles((dir, name) -> name.endsWith(".txt")); 
 
+                    //loop through b file by file
                     if (filesB != null) {
                         for (File fileB : filesB){
-                             //loop through file B by 40 to look at each record in B
-                            for (int offset = 0; offset <= 3960; offset += 40){
-                                String record = fetchRecord(fileB, offset);
-                                int randV = Integer.parseInt(record.substring(33, 37));
-                                //check join condition - loop through array 
-                                joinCondition(randV);
+                            try (FileReader frB = new FileReader(fileB)) {
+                                int fileIndexB = 0;
+                                int charIntB;
+                                String recordStr;
+                                //get entire file of b at a time
+                                char[] fileContent = new char[4000];
+                                while((charIntB = frB.read()) != -1){
+                                    fileContent[fileIndexB] = (char) charIntB;
+                                    fileIndexB++;
+                                }
+        
+                                //loop through file B by 40 to look at each record in B
+                                for (int offset = 0; offset <= 3960; offset += 40){
+                                    char[] record = Arrays.copyOfRange(fileContent, offset, offset + 40);
+                                    //get record
+                                    recordStr = new String(record);
+                                    int randV = Integer.parseInt(recordStr.substring(33, 37));
+                                    qualifyTotal += joinCondition(randV);
+                                }
+                                
+                            } catch (IOException e) {
+                                System.out.print(e.getMessage());
                             }
-
                         }
                     }
                 }catch (IOException e) {
@@ -85,9 +82,11 @@ public class NestedLoop {
     }
 
     public void join(){
-        System.out.println("hello world");
+        long startTime = System.currentTimeMillis();
         readDataset();
-        System.out.println("Number of qualifying records: " + joinCount);
+        long endTime = System.currentTimeMillis();
+        System.out.println("Number of qualifying records: " + qualifyTotal);        
+        System.out.println("Execution Time: " + (endTime - startTime) + " ms");
 
     }
 }
